4cd7b7a002348d704bf37df95897bedda2ef30b6
Willem van der Jagt <wkjagt@gmail.com>
2017-11-19 20:42:29 -0500

    remove one level of supervision at top level

--- lib/ssh_chat.ex
+++ lib/ssh_chat.ex
@@ -2,13 +2,6 @@ defmodule SshChat do
   use Application
 
   def start(_type, _args) do
-    import Supervisor.Spec
-
-    children = [
-      supervisor(SshChat.SSH.Supervisor, [])
-    ]
-
-    opts = [strategy: :one_for_one, name: SshChat.Supervisor]
-    Supervisor.start_link(children, opts)
+    SshChat.SSH.Supervisor.start_link
   end
 end
